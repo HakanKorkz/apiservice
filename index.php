c3921e53f0c255052640f27948d68da0a608de66
Ali Yılmaz <aliyilmaz.work@gmail.com>
2022-03-28 22:30:19 +0300

    1.0.1

--- index.php
+++ index.php
@@ -22,10 +22,11 @@ $conf = array(
 
 $Mind = new Mind($conf);
 
-$Mind->route('/', ['app/views/partners', 'app/views/branches', 'app/views/products'], 'app/migration/install');
+$Mind->route('/', ['app/views/partners', 'app/views/branches', 'app/views/products'], 'app/migration/restore');
 $Mind->route('new_partner', 'app/views/new_partner');
 $Mind->route('new_branch', 'app/views/new_branch');
 $Mind->route('new_product', 'app/views/new_product');
 
+$Mind->route('install', 'app/migration/install');
 $Mind->route('backup', 'app/migration/backup');
 $Mind->route('restore', 'app/migration/restore');
